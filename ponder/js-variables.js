a47ef28c5ed84ca6e338534fa36cb48644a39741
aydenkissell <aydenkissell@byui.edu>
2026-01-14 22:08:48 -0700

    Add JS variables and constants exercise

---
+++ ponder/js-variables.js
@@ -0,0 +1,13 @@
+// example of variables
+let name = "Ayden";
+let age = 24;
+
+// example of constants
+const course = "WDD 131";
+const topic = "Variables and Constants";
+
+// log values
+console.log(name);
+console.log(age);
+console.log(course);
+console.log(topic);
